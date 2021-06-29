125f49610f945dee398d4a8335a797d93b798b6c
Desalegn <tigistu_desalgn25@yahoo.com>
2021-06-29 03:02:22 -0400

    first commit

---
+++ student.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+  
+})
+export class StudentComponent implements OnInit {
+
+ShowMe:Boolean
+  constructor() { 
+    this.ShowMe=true
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+  toogleTag(){
+    this.ShowMe=!this.ShowMe
+  }
+
+}
